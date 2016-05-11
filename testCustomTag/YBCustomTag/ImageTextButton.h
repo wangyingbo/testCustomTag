9b39b518df925af564d9eaf644f71e065328b107
wangyingbo <wangyingbo528@126.com>
2016-05-11 12:23:19 +0800

    优化细节

---
+++ testCustomTag/YBCustomTag/ImageTextButton.h
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  testCustomTag
+//
+//  Created by 王迎博 on 16/4/28.
+//  Copyright © 2016年 王迎博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum {
+    UIButtonTitleWithImageAlignmentUp = 0,  // title is up
+    UIButtonTitleWithImageAlignmentLeft,    // title is left
+    UIButtonTitleWithImageAlignmentDown,    // title is down
+    UIButtonTitleWithImageAlignmentRight    // title is right
+} UIButtonTitleWithImageAlignment;
+
+@interface ImageTextButton : UIButton
+
+
+//图片 文字间距
+@property (nonatomic) CGFloat imgTextDistance;  // distance between image and title, default is 5
+@property (nonatomic) UIButtonTitleWithImageAlignment buttonTitleWithImageAlignment;  // need to set a value when used
+
+- (UIButtonTitleWithImageAlignment)buttonTitleWithImageAlignment;
+- (void)setButtonTitleWithImageAlignment:(UIButtonTitleWithImageAlignment)buttonTitleWithImageAlignment;
+
+
+
+
+@end
